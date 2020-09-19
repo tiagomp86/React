1abdf8a422257ef59b283987e78188e80245c658
Müller <tiagomp986@gmail.com>
2020-09-19 11:23:28 -0300

    Modified project - adjust button name

--- src/Incremento/Incremento.jsx
+++ src/Incremento/Incremento.jsx
@@ -4,7 +4,7 @@ export default (props) => {
     return (
         <div>
                 <input type="numeric" onChange={props.multiplicadorOnChange} value={props.multiplicador}></input>
-                <button onClick={props.buttonClick}>Incrementar</button>
+                <button onClick={props.buttonClick}>Somar</button>
                 <p>Valor: {props.valor}</p>
         </div>
     );
